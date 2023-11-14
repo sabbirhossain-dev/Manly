a5c7fbb0a81b6332d211fc82072dfdcaf085be55
Md Sabbir Hossain <74786010+sabbirhossain-dev@users.noreply.github.com>
2023-11-14 08:38:23 +0600

    Added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// const navLinks = document.querySelectorAll('.nav-link');
+
+// navLinks.forEach(navElement =>{
+//     navElement.addEventListener('click', function() {
+//         document.querySelector('.active')?.classList.remove('active');
+//         this.classList.add('active');
+//     })
+// })
